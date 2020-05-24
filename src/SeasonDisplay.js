bb4c0e1a3ba50462f119843ec71b6b1255915959
Grishmin karki <grishmink@gmail.com>
2020-05-24 17:18:10 +0545

    season app

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,16 @@
+import "./style.css";
 import React from "react";
+
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Burr it is cold",
+    iconName: "snowflake",
+  },
+};
 
 //in northern hemisphere(lat>0)- 3 to 8(summer),0 to 2 and 9 to 11(winter)
 //in southern hemisphere, just opposite
@@ -13,11 +25,18 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
 
-  const text =
-    season === "winter" ? "Burr, it is chilly" : "Lets hit the beach";
+  const { text, iconName } = seasonConfig[season];
+
+  // const text =
+  //   season === "winter" ? "Burr, it is chilly" : "Lets hit the beach";
+
+  // const icon = season === "winter" ? "snowflake" : "sun";
+
   return (
-    <div>
+    <div className={`wrapper ${season}`}>
+      <i className={`${iconName} icon massive icon-left`} />
       <h1>{text}</h1>
+      <i className={`${iconName} icon massive icon-right`} />
     </div>
   );
 };
